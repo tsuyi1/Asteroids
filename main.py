26ca305e980bdf2cddc204276cc4c4088407a059
tsuyi1 <jesus.carrillot24@gmail.com>
2025-07-28 17:20:20 -0600

    add constants.py and player.py

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 import pygame
 from constants import *
+from player import * 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    running = True
+    clock = pygame.time.Clock()
+    player = Player((SCREEN_WIDTH/2),(SCREEN_HEIGHT/2))
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
+        player.draw(screen)
         pygame.display.flip()
-    #print("Starting Asteroids!")
-    #print(f"Screen width: {SCREEN_WIDTH}")
-    #print(f"Screen height: {SCREEN_HEIGHT}")
+
+        #limit the framerate to 60fps
+        dt = clock.tick(60)/1000
+    
 
 if __name__ == "__main__":
     main()
