3c1eb0bbc3ef93a51938eac83ccedd2d96a3943b
tsuyi1 <jesus.carrillot24@gmail.com>
2025-07-30 14:42:58 -0600

    add shoot cooldown

--- player.py
+++ player.py
@@ -7,6 +7,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y,20)
         self.rotation = 0
+        self.timer = 0
     
     # in the player class
     def triangle(self):
@@ -28,9 +29,11 @@ class Player(CircleShape):
         self.position += forward * PLAYER_SPEED * dt
 
     def shoot(self):
-        bullet = Shot(self.position.x,self.position.y,SHOT_RADIUS) 
-        shot_velocity = pygame.Vector2(0,1).rotate(self.rotation)
-        bullet.velocity = PLAYER_SHOOT_SPEED * shot_velocity
+        if self.timer <= 0:
+            bullet = Shot(self.position.x,self.position.y,SHOT_RADIUS) 
+            shot_velocity = pygame.Vector2(0,1).rotate(self.rotation)
+            bullet.velocity = PLAYER_SHOOT_SPEED * shot_velocity
+            self.timer = PLAYER_SHOOT_COOLDOWN
 
 
     def update(self, dt):
@@ -44,6 +47,8 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
+        if self.timer > 0:
+            self.timer -= dt
         if keys[pygame.K_SPACE]:
             self.shoot()
 
